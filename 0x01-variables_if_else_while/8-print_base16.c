f93271a5c2a9a349e7dacd83d87ed5db6c6d324d
OZOUMEEMMANUEL <ozoumeemmanuel18@gmail.com>
2022-09-09 17:32:49 -0700

    task 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * 
+ *Return: 0(sucess)
+ */
+
+int main(void)
+{
+	int d;
+	char low;
+	for(d='0'; d<='9'; d++){
+		putchar(d);}
+	for(low='a'; low<='f'; low++){
+		putchar(low);}
+	putchar('/n');
+	return 0;
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+                                                                                                                                  
+#include <stdlib.h>                                                                                                               
+#include <time.h>                                                                                                                 
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return = 0 (success)
+ */
+
+int main(void)
+{
+	int d;
+	for(d='0'; d<='9'; d++){
+		putchar(d);}
+	if (d !='9'){
+		putchar(',');
+		putchar(' ');}
+}
+putchar('/n');
+return 0;
