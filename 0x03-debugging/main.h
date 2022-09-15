deea29b056d96124daa9ba661845b25a5902a8d3
OZOUMEEMMANUEL <ozoumeemmanuel18@gmail.com>
2022-09-15 04:02:50 -0700

    main.h correction

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
 #include<stdio.h>
 
@@ -8,4 +8,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H_INCLUDED */
+#endif /* MAIN_H */
