209b47f4ea4701ba6af70b527211920877a8e75a
OZOUMEEMMANUEL <ozoumeemmanuel18@gmail.com>
2022-09-15 02:48:05 -0700

    Write a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/*
+ * print_square - a function that prints a square, followed by a new line
+ *@size: size of both width and length
+ *Return: a square made of '#'
+ */
+void print_square(int size)
+{
+	int co, ro;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= size; co++)
+		{
+			putchar('#');
+			for (ro = 2; ro <= size; ro++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
